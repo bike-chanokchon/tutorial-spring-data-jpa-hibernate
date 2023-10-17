31de4378b198169efb576633f5cc268a7401004a
Colorfuls <bike.chanokchon2001@gmail.com>
2023-10-17 11:17:32 +0700

    initial project

---
+++ sdjpa-into/src/main/java/com/springframework/sdjpainto/SdjpaIntoApplication.java
@@ -0,0 +1,13 @@
+package com.springframework.sdjpainto;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SdjpaIntoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SdjpaIntoApplication.class, args);
+	}
+
+}
