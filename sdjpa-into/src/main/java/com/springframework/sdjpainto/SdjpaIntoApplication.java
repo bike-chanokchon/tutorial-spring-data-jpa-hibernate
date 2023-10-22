349fdd1a57291d4fd58f87cce0f9825d653bdf82
Chanokchon Wongjumpa <bike.chanokchon2001@gmail.com>
2023-10-22 17:42:28 +0700

    add test case

--- sdjpa-into/src/main/java/com/springframework/sdjpainto/SdjpaIntoApplication.java
+++ sdjpa-into/src/main/java/com/springframework/sdjpainto/SdjpaIntoApplication.java
@@ -2,8 +2,10 @@ package com.springframework.sdjpainto;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
+@ComponentScan(basePackages = {"com.springframework.sdjpainto.bootstrap"})
 public class SdjpaIntoApplication {
 
 	public static void main(String[] args) {
