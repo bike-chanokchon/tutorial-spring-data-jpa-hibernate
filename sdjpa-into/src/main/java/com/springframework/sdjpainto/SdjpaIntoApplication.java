61aa2e75f1bb65c9a205455eb432241488035374
Colorfuls <bike.chanokchon2001@gmail.com>
2023-10-27 14:06:27 +0700

    fix cannot run test with environment

--- sdjpa-into/src/main/java/com/springframework/sdjpainto/SdjpaIntoApplication.java
+++ sdjpa-into/src/main/java/com/springframework/sdjpainto/SdjpaIntoApplication.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
-@ComponentScan(basePackages = {"com.springframework.sdjpainto.bootstrap"})
+// @ComponentScan(basePackages = {"com.springframework.sdjpainto.bootstrap"})
 public class SdjpaIntoApplication {
 
 	public static void main(String[] args) {
